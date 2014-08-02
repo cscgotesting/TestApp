44d75d8adbf261da35046e7d4c144955d60eceb5
cscgotesting <arunasokkumar@gmail.com>
2014-08-02 21:37:33 +0530

    New test

---
+++ TestApp/test/test/SampleTest.java
@@ -0,0 +1,29 @@
+package test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.csc.util.MyTest;
+
+public class SampleTest {
+
+	
+	MyTest test;
+	
+	/*@Before
+	public void init(){
+		test = new MyTest();
+	}*/
+	
+	
+	@Test
+	public void testSample(){
+		test = new MyTest();
+		
+		Assert.assertEquals("My Java Test Program", test.getMessage());
+		
+		test.setMessage("hello");
+		
+		Assert.assertEquals("hello", test.getMessage());
+	}
+}
